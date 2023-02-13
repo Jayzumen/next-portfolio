4a4d5d9fafab3024f3ad5b682199f2699964d4bd
jayzumen <reinhardtjanniklas96@gmail.com>
2023-02-13 20:55:41 +0100

    changed fetch

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,12 +9,15 @@ interface Project {
   description: string;
   html_url: string;
   created_at: string;
+  stargazers_count: number;
 }
 
 const fetchProjects = async (): Promise<Project[]> => {
   const response = await fetch("https://api.github.com/users/Jayzumen/repos");
   const data: Project[] = await response.json();
-  const filteredData: Project[] = data.filter((repo) => repo.homepage);
+  const filteredData: Project[] = data.filter(
+    (repo) => repo.homepage && repo.description && repo.stargazers_count > 0
+  );
   filteredData.sort((a, b) => {
     const dateA: Date = new Date(a.created_at);
     const dateB = new Date(b.created_at);
