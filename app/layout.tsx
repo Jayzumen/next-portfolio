18c93d2ea24039440f13823a93d952221e432fb6
jayzumen <reinhardtjanniklas96@gmail.com>
2023-02-18 18:07:50 +0100

    removed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
-import { AnalyticsWrapper } from "./components/analytics";
 
 export default function RootLayout({
   children,
@@ -15,7 +14,6 @@ export default function RootLayout({
         <main className="mx-auto mt-4 w-full rounded-md bg-zinc-300 p-4 text-black shadow-md shadow-zinc-600 md:max-w-[80%]">
           {children}
         </main>
-        <AnalyticsWrapper />
       </body>
     </html>
   );
